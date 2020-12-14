a8adad59d2d0f3a149e399695a9c2afc773d3312
Marco Chacon <marco.chacon1796@gmail.com>
2020-12-14 11:25:56 -0600

    adding generic views

--- polls/views.py
+++ polls/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html' # change the default: <app name>/<model name>_list.html
+    context_object_name = 'latest_question_list' # changes default context variable (context_object_name): question_list
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    # question = get_object_or_404(Question, pk=question_id) ###alternative
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# expects pk to retrieve the particular item
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html' #change the default: <app name>/<model name>_detail.html
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# solve race problem
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
